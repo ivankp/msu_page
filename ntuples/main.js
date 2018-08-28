96a3d568d4a9a2e41614c0bd9b170d0fba4a6aec
ivankp <ivan.pogrebnyak@gmail.com>
2018-08-28 00:34:19 -0400

    ntuples: working selectors

--- ntuples/main.js
+++ ntuples/main.js
@@ -1,4 +1,5 @@
 var doc = document;
+var ii = [ ];
 
 $.prototype.el = function(tag,text=null) {
   var dom = doc.createElement(tag);
@@ -9,44 +10,33 @@ $.prototype.el = function(tag,text=null) {
 
 $(function() {
   var sel = $('#sel');
+  var leg = sel.parent().parent().parent().find('legend');
 
   var sel_set = sel.el('select').attr('size',10).attr('id','sel-set');
   hist_sets.forEach(x => { sel_set.el('option',x[0]); });
 
   sel_set.change(function() {
-    const index = this.selectedIndex;
+    const index = ii[0] = this.selectedIndex;
 
     var sel_hist = $('#sel-hist');
-    if (!sel_hist.length)
+    if (!sel_hist.length) {
       sel_hist = sel.el('select').attr('size',10).attr('id','sel-hist');
-    else sel_hist.empty();
+      sel_hist.change(function() {
+        const index = ii[1] = this.selectedIndex;
+        leg.text(hist_sets[ii[0]][0]+' : '+hist_sets[ii[0]][1][index]);
+      });
+    } else sel_hist.empty();
     hist_sets[index][1].forEach(x => { sel_hist.el("option",x); });
 
     hist_sets[index][2].forEach(x => {
       const id = x[0];
       var sel_bin = $('#'+id);
       if (!sel_bin.length)
-        sel_bin = sel.el('select').attr('id',id);
+        sel_bin = sel.el('select').attr('id',id).css({'display': 'block'});
       else sel_bin.empty();
-      x[1].forEach(x => { sel_bin.el("option",x); });
+      x[1].forEach(x => { sel_bin.el('option',x); });
     });
-  });
 
-  // hist_sets = hist_sets.slice(1).map(
-  //   row => {
-  //     var result = {};
-  //     hist_sets[0].forEach((key,i) => result[key] = row[i]);
-  //     return result;
-  //   }
-  // );
-  //
-  // hist_sets.forEach(x => { $('#sel-set').make("option",x['name']); });
-  // $('#sel-set').change(function() {
-  //   var hsel = $('#sel-hist');
-  //   hsel.empty();
-  //   hist_sets[this.selectedIndex]['hists'].forEach(x => {
-  //     hsel.make("option",x);
-  //   });
-  // });
-  // $('#select-set').make("option",'dummy');
+    leg.text($(this).val());
+  });
 });
--- ntuples/update_hist_sets.py
+++ ntuples/update_hist_sets.py
@@ -3,17 +3,10 @@
 import json, glob, re
 from subprocess import Popen, PIPE
 
-# out = [ ['set', 'hists', 'bins', 'info'] ]
-
 out = [ ]
 
 for fname in glob.glob('data/*.json.xz'):
     j = json.loads(Popen(["xz","-dc",fname],stdout=PIPE).communicate()[0])
-    # out[re.sub(r'.*/(.*)\.json\.xz',r'\1',fname)] = {
-    #     "hists": sorted([ h for h in j['histograms'] ]),
-    #     "bins" : j['annotation']['bins'],
-    #     "info" : {'jets': j['annotation']['runcard']['analysis']['jets']}
-    # }
     out.append([
         re.sub(r'.*/(.*)\.json\.xz',r'\1',fname),
         sorted([ h for h in j['histograms'] ]),
