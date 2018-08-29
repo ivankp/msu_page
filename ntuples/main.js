27040b4ab728a822b1ee054818f100734e2d71fe
ivankp <ivan.pogrebnyak@gmail.com>
2018-08-28 22:04:33 -0400

    ntuples: histograms work

--- ntuples/main.js
+++ ntuples/main.js
@@ -12,26 +12,37 @@ $.prototype.el = function(tag,text=null) {
 
 function update_hist() {
   leg.text(menu.file+' : '+menu.hist);
-  const hist_data = data[menu.file].histograms[menu.hist].bins.map(
+  const hist_data = data[menu.file].histograms[menu.hist];
+  const hist_axes = hist_data.axes;
+  const hist_bins = hist_data.bins.map(
     x => x==null ? null : x[0][0][0]
-  );
-  console.log(hist_data);
+  ).slice(1,-1);
 
-  let svg = make_svg('#plot',400,250);
+  if (hist_axes.length!=1) return;
+
+  const yrange = d3.extent(hist_bins.map(x => x[0]));
+  var xedge = function(i) {
+    const a = hist_axes[0].range[0];
+    const b = hist_axes[0].range[1];
+    const n = hist_axes[0].nbins;
+    return a + i*(b-a)/n;
+  };
+
+  let svg = make_svg('#plot',788,533);
   let canv = canvas(svg, [
-    { range: [105,160], padding: [33,10], label: menu.hist },
-    { range: [0,d3.max([1])*1.05], padding: [45,5] }
+    { range: hist_axes[0].range, padding: [33,10], label: menu.hist },
+    { range: [yrange[0]*0.95,yrange[1]*1.05], padding: [45,5] }
   ]);
-  // hist('histogram', canv, bin.hist.map(
-  //   (x,i) => [ 105+i, 106+i, x, Math.sqrt(x) ]
-  // ),{
-  //   color: '#000099',
-  //   width: 2
-  // });
+  hist('histogram', canv, hist_bins.map(
+    (x,i) => [ xedge(i), xedge(i+1), x[0], Math.sqrt(x[1]) ]
+  ),{
+    color: '#000099',
+    width: 2
+  });
 }
 
 $(function() {
-  var div = $('#sel');
+  const div = $('#sel');
   leg = div.parent().parent().parent().find('legend');
 
   var fsel = div.el('select').attr('size',10).attr('id','fsel');
@@ -48,7 +59,7 @@ $(function() {
           menu.hist = this.value;
           update_hist();
         });
-      } else hsel.empty();
+      35} else hsel.empty();
       Object.keys(file.histograms).forEach(x => { hsel.el("option",x); });
 
       file.annotation.bins.forEach(x => {
@@ -60,27 +71,25 @@ $(function() {
         x[1].forEach(x => { sel_bin.el('option',x); });
       });
 
-      if (menu.hist != null)
+      if (menu.hist)
         if (hsel.find('option').filter((i,e) => e.text == menu.hist)
           .prop('selected', true).trigger('change').length) return;
       leg.text(fname);
     };
 
     if (!(fname in data)) {
-      fsel.prop("disabled", true);
-      hsel.prop("disabled", true);
+      div.find('select').prop("disabled", true);
       fetch(data_path(fname)).then(r => r.arrayBuffer()).then(buf => {
         LZMA.decompress(
           new Uint8Array(buf),
           function(result, error) {
             const file = data[fname] = JSON.parse(result);
             update(file);
-            fsel.prop("disabled", false);
-            hsel.prop("disabled", false);
+            div.find('select').prop("disabled", false);
           }
         )
       });
     } else update(data[fname]);
-  }); // end fsel.change()
+  }); // end fsel change
 });
 
