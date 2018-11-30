9003bff24184253baa01918d54a241e32efeda5d
ivankp <ivan.pogrebnyak@gmail.com>
2018-11-29 19:23:44 -0500

    hgam table: HT and x = pT/HT

--- pages/hgam/table/req.php
+++ pages/hgam/table/req.php
@@ -2,6 +2,6 @@
 $path = '/home/ivanp/public_html/';
 echo exec(
   'echo \''.json_encode($_POST,JSON_NUMERIC_CHECK).'\' | '.
-  'LD_LIBRARY_PATH='.$path.'lib '.'bin/explorer '.$path.'hgam_data.dat 2>&1'
+  'LD_LIBRARY_PATH='.$path.'lib '.'bin/table '.$path.'hgam_data.dat 2>&1'
 );
 ?>
