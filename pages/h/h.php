e0d6e4b1721f9d3dadcf3bc3aaa3c972835788fb
ivankp <ivan.pogrebnyak@gmail.com>
2018-12-15 23:00:03 -0500

    new hist page 2

--- pages/h/h.php
+++ pages/h/h.php
@@ -3,12 +3,12 @@
 <script src="js/d3-selection-multi.v1.min.js"></script>
 <script>
 const dir = "<?php echo $dir;?>";
-const files = [<?php
-$files_dir = $dir.'/files';
+var files = [<?php
+$files_dir = "$dir/files";
 foreach (scandir($files_dir) as $f) {
   $m = array();
   if (!preg_match('/^(.+)\.dat\.xz$/',$f,$m)) continue;
-  echo '"'.$m[1].'",';
+  echo "\"$m[1]\",";
 }
 ?>];
 </script>
@@ -18,22 +18,27 @@ foreach (scandir($files_dir) as $f) {
 <div class="form_col">
 <p>Select file</p>
 <input type="search" name="file_filter" placeholder="Filter..." value="NLO">
-<select name="file_select" size="10"></select>
+<select name="file_select" size="10" class="tall_select"></select>
 </div>
 <div class="form_col">
 <p>Select histogram</p>
 <input type="search" placeholder="Filter..." name="hist_filter">
-<select name="hist_select" size="10"></select>
+<select name="hist_select" size="10" class="tall_select"></select>
 </div>
 </div>
 <div class="form_row">
 <div class="form_col" id="cats">
 <p>Options</p>
 </div>
+<div class="form_col">
+<p>&nbsp;</p>
+<label><input id="logy" type="checkbox">log y</label><br>
+<label><input id="divbinw" type="checkbox">&divide;width</label>
+</div>
 </div>
 </div>
 
-<div id="plot">TEST</div>
+<div id="plot"></div>
 
 <script src="<?php echo $dir;?>/plot.js"></script>
 <script src="<?php echo $dir;?>/main.js"></script>
--- pages/h/main.js
+++ pages/h/main.js
@@ -4,7 +4,13 @@ $.prototype.el = function(tag,text=null) {
   this.append(dom);
   return $(dom);
 }
-function bind(f,...x) { return function(){ f(...x); } }
+function bind(f,...x) { return function(...y){ f(...x,...y); } }
+
+function indices(n) {
+  let a = [ ];
+  for (let i=0; i<n; ++i) a.push(i);
+  return a;
+}
 
 function select(name) { return $('select[name='+name+'_select]'); }
 function filter(name) { return $('input[name='+name+'_filter]'); }
@@ -26,11 +32,25 @@ function set_options(name,opts) {
 function safe_file_val() {
   const sel = select('file');
   let f = sel.val();
-  if (!f) {
-    $(sel[0][0]).prop('selected',true);
-    f = sel.val();
+  if (f) return f;
+  if (files.cur) {
+    if (sel.children().filter(function(){
+      return this.text == files.cur;
+    }).prop('selected',true).length != 1) {
+      sel.el('option',files.cur).prop('selected',true);
+    }
+    return files.cur;
   }
-  return f;
+  if (sel.length > 0) return $(sel[0][0]).prop('selected',true).text;
+  return null;
+}
+
+function hist_req(name) {
+  const sels = $('#cats').find('select');
+  let req = { 'hist': name };
+  for (const sel of sels)
+    req[sel.name] = sel.value;
+  return req;
 }
 
 function post(req,done,focus) {
@@ -39,69 +59,104 @@ function post(req,done,focus) {
     url: dir+"/req.php",
     data: { 'file': safe_file_val(), 'do': req },
     beforeSend: function() { $('#form :input').prop('disabled',true); },
-    success: function(resp){
+    dataFilter: function(resp) {
       if (resp) {
         try {
-          resp = JSON.parse(resp);
+          return JSON.parse(resp);
         } catch(e) {
           alert('Bad server response: '+resp);
           console.log(resp);
-          resp = false;
         }
-        if (resp) done(resp);
       } else alert('Empty server response');
+      return false;
+    },
+    success: function(resp){
+      if (resp) done(resp);
       $('#form :input').prop('disabled',false);
       if (focus) focus.focus();
     }
   });
 }
 
-const hists = { all: [ ], cur: null };
+files = { all: files, cur: null, loaded: { } };
+const hists = { all: [ ], cur: null, loaded: { } };
+
+function update_file(resp){
+  hists.all.length = 0;
+  hists.all.push(...resp);
+  const sel = set_options('hist',hists.all);
+  if (hists.cur) sel.val(hists.cur).trigger('change');
+}
+
+function update_hist(resp) {
+  const hist_name = Object.keys(resp.hists)[0];
+  const hist = resp.hists[hist_name];
+  const cats_col = $('#cats').find('select').remove().end();
+  const cats = hist['categories'];
+  for (const cat in cats) {
+    const sel = cats_col.el('select').prop({'name':cat,'title':cat});
+    let i = hist['selection'][cat];
+    for (let opt of cats[cat]) {
+      opt = sel.el('option',opt);
+      if (i--==0) opt.prop('selected',true);
+    }
+    sel.on('change',function(){
+      post(hist_req(hist_name),function(resp){
+        console.log(resp);
+      },$(this));
+    });
+  }
+  console.log(JSON.stringify(hist));
+
+  const xi = 0;
+  const xrange = ['min','max'].map(x => hist.axes[xi][x]);
+  const xn = hist.axes[xi].nbins;
+  const xw = (xrange[1]-xrange[0])/xn;
+  const xedge = function(i) { return xrange[0] + i*xw; };
+
+  const yrange = plot.hist_yrange(
+    d3.extent(hist.bins.filter(x => x!==null).map(x => x[0])));
+  console.log(yrange);
+
+  const logy = $('#logy').prop('checked');
+
+  const svg = plot.make_svg('#plot',788,533,'white')
+    .attr('version','1.1')
+    .attr('xmlns','http://www.w3.org/2000/svg');
+  const canv = plot.canvas(svg, [
+    { range: xrange, padding: [43,10], label: hist_name,
+      values: xn < 12 ? indices(xn+1).map(i=>xedge(i)) : null
+    },
+    { range: yrange, padding: [45,5], log: logy,
+      label: 'cross section'
+      // label: (factor<0 ? '\u2212 ' : '') + 'cross section [' + units() + ']'
+    }
+  ]);
+}
 
 $(function(){
-  set_options('file',files);
-  filter('file').on('input',bind(set_options,'file',files));
+  set_options('file',files.all);
+  filter('file').on('input',bind(set_options,'file',files.all));
   filter('hist').on('input',bind(set_options,'hist',hists.all));
 
   select('file').on('change',function(){
-    post('list',function(resp){
-      hists.all.length = 0;
-      hists.all.push(...resp);
-      const sel = set_options('hist',hists.all);
-      if (hists.cur) sel.val(hists.cur).trigger('change');
-    },$(this));
+    const file_name = files.cur = $(this).val();
+    if (files.loaded.hasOwnProperty(file_name)) {
+      update_file(files.loaded[file_name]);
+    } else post('list',update_file,$(this)).done(resp => {
+      files.loaded[file_name] = resp;
+    });
   });
 
-  const cats_col = $('#cats');
-  function hist_req(name) {
-    const sels = cats_col.find('select');
-    let req = { 'hist': name };
-    for (const sel of sels)
-      req[sel.name] = sel.value;
-    return req;
-  }
   select('hist').on('change',function(){
     const hist_name = hists.cur = $(this).val();
-    post(hist_req(hist_name),function(resp){
-      const hist = resp['hists'][hist_name];
-      cats_col.find('select').remove();
-      const cats = hist['categories'];
-      for (const cat in cats) {
-        const sel = cats_col.el('select').prop({'name':cat,'title':cat});
-        let i = hist['selection'][cat];
-        for (let opt of cats[cat]) {
-          opt = sel.el('option',opt);
-          if (i--==0) opt.prop('selected',true);
-        }
-        sel.on('change',function(){
-          post(hist_req(hist_name),function(resp){
-            console.log(resp);
-          },$(this));
-        });
-      }
-      console.log(JSON.stringify(resp));
-      console.log(JSON.stringify(hist['bins']));
-    },$(this));
+    const req = hist_req(hist_name);
+    const req_str = JSON.stringify([safe_file_val(),Object.values(req)]);
+    if (hists.loaded.hasOwnProperty(req_str)) {
+      update_hist(hists.loaded[req_str]);
+    } else post(req,update_hist,$(this)).done(resp => {
+      hists.loaded[req_str] = resp;
+    });
   });
 });
 
